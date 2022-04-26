614b422f488377a525f6db347692ea6c3fa534da
robertd23 <robertd23@unc.edu>
2022-04-25 23:20:31 -0400

    a millionth try45

--- flips.js
+++ flips.js
@@ -4,5 +4,10 @@ import minimist from "minimist";
 var argum = (process.argv.slice(2));
 argum['number'];
 let number = argum.number || 1;
-let list = coinFlips(number)
-console.log(list, countFlips(list))
+if (number> 0) {
+    let list = coinFlips(number)
+    console.log(list, countFlips(list))
+}
+else {
+    
+}
