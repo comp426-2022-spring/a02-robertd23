88db45e42bfe2b7969fda03f450f8e633504c587
robertd23 <robertd23@unc.edu>
2022-04-25 22:57:06 -0400

    a millionth try

--- guess-flip.js
+++ guess-flip.js
@@ -1,7 +1,9 @@
 import { flipACoin } from "./modules/coin.mjs";
 import minimist from "minimist";
+import { createRequire} from 'module';
+const require = createRequire(import.meta.url);
 
-var argum = minimist(process.argv.slice(2));
+var argum = require("minimist")(process.argv.slice(2));
 var input = argum['call']
 if (input == null) {
     console.log("Error: no input")
