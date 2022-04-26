e6c1000adc13adde4036dd226a930c727b8a92f7
robertd23 <robertd23@unc.edu>
2022-04-25 22:53:44 -0400

    a millionth try

--- flips.js
+++ flips.js
@@ -1,7 +1,9 @@
 import { coinFlips, countFlips } from "./modules/coin.mjs";
 import minimist from "minimist";
+import { createRequire} from 'module';
+const require = createRequire(import.meta.url);
 
-var argum = minimist(process.argv.slice(2));
+var argum = require("minimist")(process.argv.slice(2));
 var input = argum['number'] || 1;
 let list = coinFlips(input)
 console.log(list)
